5d331a1809d2593a84e99fa06883b1f34fbab53c
kelleyrw <kelleyrw@users.noreply.github.com>
2022-03-02 07:23:22 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-from setuptools import setup
 import os
+
+import setuptools
+from setuptools import setup
+
 
 def get_long_description():
     with open(
-        os.path.join(os.path.dirname(__file__), "README.md"),
-        encoding="utf8"
+        os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf8"
     ) as fp:
         return fp.read()
 
 
 setup(
     name="data_science_common",
-    version=os.getenv('VERSION'),
+    version=os.getenv("VERSION"),
     description="UNDER CONSTRUCTION: A simple python library for facilitating analysis",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
